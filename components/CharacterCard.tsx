f54e5615b3526da2cd5dde847b909dbc77208f28
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2023-07-03 00:01:43 +0300

    Making character card UX better when fetching

--- components/CharacterCard.tsx
+++ components/CharacterCard.tsx
@@ -3,35 +3,33 @@
 import useSearchCharacter from "@/hooks/useSearchCharacter";
 
 function CharacterCard() {
-  const { data, isLoading } = useSearchCharacter();
+  const { data, isFetching } = useSearchCharacter();
+
+  if (isFetching) return <div>Loading...</div>;
+
+  if (!data) return <div></div>;
 
   return (
     <div>
-      {isLoading ? (
-        <div>Loading...</div>
-      ) : (
-        <>
-          <img
-            className="rounded-full"
-            src={data?.thumbnail_url}
-            alt="character thumbnail"
-          />
-          <p>
-            {data?.name} - {data?.active_spec_name}
-            {" " + data?.class}
-          </p>
-          <p>
-            {data?.realm} - {data?.region}
-          </p>
-          <p>{data?.achievement_points} Achievement Points</p>
-          <p>
-            {data?.mythic_plus_scores_by_season[0].scores.all} Mythic+ Score
-          </p>
-          <p>
-            {data?.guild.name} - {data?.guild.realm}
-          </p>
-        </>
-      )}
+      <>
+        <img
+          className="rounded-full"
+          src={data?.thumbnail_url}
+          alt="character thumbnail"
+        />
+        <p>
+          {data?.name} - {data?.active_spec_name}
+          {" " + data?.class}
+        </p>
+        <p>
+          {data?.realm} - {data?.region}
+        </p>
+        <p>{data?.achievement_points} Achievement Points</p>
+        <p>{data?.mythic_plus_scores_by_season[0].scores.all} Mythic+ Score</p>
+        <p>
+          {data?.guild.name} - {data?.guild.realm}
+        </p>
+      </>
     </div>
   );
 }
