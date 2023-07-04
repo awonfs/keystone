7ab5e23d4e0c147f1adf24a05c849841ab6bd152
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2023-07-04 21:37:47 +0300

    Implemented list of recently completed dungeons

--- components/RecentRuns.tsx
+++ components/RecentRuns.tsx
@@ -15,9 +15,7 @@ import {
   TableRow,
 } from "@/components/ui/table";
 
-import { Switch } from "@/components/ui/switch";
 import { Star } from "lucide-react";
-import { useState } from "react";
 
 interface RecentRunsProps {
   recentRuns: [
@@ -31,28 +29,13 @@ interface RecentRunsProps {
 }
 
 function RecentRuns({ recentRuns }: RecentRunsProps) {
-  const [showUntimedRuns, setShowUntimedRuns] = useState(false);
-
-  const completedRunsOnTime = recentRuns.filter(
-    (run) => run.num_keystone_upgrades && run.num_keystone_upgrades > 0
-  );
-
-  const runsToDisplay = showUntimedRuns ? recentRuns : completedRunsOnTime;
-
   return (
     <Accordion type="single" collapsible>
       <AccordionItem value="item-1">
         <AccordionTrigger>
-          <h1>Recently timed runs</h1>
+          <h1>Recently completed dungeons</h1>
         </AccordionTrigger>
         <AccordionContent>
-          <div className="flex items-center justify-end gap-2 mb-1 w-full">
-            <Switch
-              checked={showUntimedRuns}
-              onCheckedChange={setShowUntimedRuns}
-            />
-            <span>Show untimed runs</span>
-          </div>
           <Table>
             <TableHeader>
               <TableRow>
@@ -62,7 +45,7 @@ function RecentRuns({ recentRuns }: RecentRunsProps) {
               </TableRow>
             </TableHeader>
             <TableBody>
-              {runsToDisplay.map((run) => {
+              {recentRuns.map((run) => {
                 return (
                   <TableRow key={run.dungeon}>
                     <TableCell>{run.dungeon}</TableCell>
--- todo.md
+++ todo.md
@@ -1,5 +1,4 @@
 1. Create a component that shows recent mythic+ runs from the character that the user searched. #Done
-   1.1 Switch button for showing / hiding uncompleted runs. ---> This has been added but is still bugged slightly.
 2. Create a page where user can save their own characters and see their recent mythic+ runs.
    2.1 Install drizzle ORM and connect to a planetscale database.
    2.2 Create a schema for users that has the following properties:
