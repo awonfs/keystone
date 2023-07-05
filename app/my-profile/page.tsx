a93b8a04381e43e166dd4b6ea71ac7a92ddd9c09
Eetu Rönkkö <eetu.kristian.ronkko@gmail.com>
2023-07-05 19:14:45 +0300

    Started working on ProfilePage

--- app/my-profile/page.tsx
+++ app/my-profile/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
+
 function MyProfilePage() {
   return (
     <div className="container flex flex-col items-center justify-center">
-      MyProfilePage
+      <header className="flex items-center justify-evenly w-full mt-4">
+        <h1 className="text-4xl font-bold">My Profile</h1>
+        <Button>Add a character</Button>
+      </header>
     </div>
   );
 }
