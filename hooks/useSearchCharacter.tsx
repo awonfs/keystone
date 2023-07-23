00ff0812c50462296951ee15c090a3e3910931a0
Eetu Rönkkö <105866472+awonfs@users.noreply.github.com>
2023-07-24 00:25:57 +0300

    conflict updates #2

--- hooks/useSearchCharacter.tsx
+++ hooks/useSearchCharacter.tsx
@@ -2,40 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import axios from "axios";
 import { useAtom } from "jotai";
 import { characterFormDataAtom } from "@/atoms/characterFormDataAtom";
-
-export interface CharacterData {
-  name: string | undefined;
-  race: string | undefined;
-  class: string | undefined;
-  active_spec_name: string | undefined;
-  achievement_points: number | undefined;
-  thumbnail_url: string | undefined;
-  region: string | undefined;
-  realm: string | undefined;
-  mythic_plus_scores_by_season: [
-    {
-      season: number | undefined;
-      scores: {
-        all: number | undefined;
-      };
-    }
-  ];
-  guild: {
-    name: string | undefined | null;
-    realm: string | undefined | null;
-  };
-  gear: {
-    item_level_equipped: number | undefined;
-  };
-  mythic_plus_recent_runs: [
-    {
-      dungeon: string | undefined;
-      short_name: string | undefined;
-      mythic_level: number | undefined;
-      num_keystone_upgrades: number | undefined;
-    }
-  ];
-}
+import { CharacterData } from "@/utils/interfaces/characterData";
 
 function wait() {
   return new Promise((resolve) => setTimeout(resolve, 2000));
